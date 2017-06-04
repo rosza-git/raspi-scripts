654ce2a085f96f0db2b6a4a2ba8252dbdf8e5a0e
rosza <roland.szalay.81@gmail.com>
2017-06-04 15:49:52 +0200

    updated with log and info

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-echo -e "\n#####################################"
-echo "#                                   #"
-echo "#  RaspberryPi system informations  #"
-echo "#                                   #"
-echo "#####################################"
-echo -e "\nCPU frequency     : `vcgencmd measure_clock arm`"
-echo "CORE frequency    : `vcgencmd measure_clock core`"
-echo "CORE voltage      : `vcgencmd measure_volts core`"
-echo "CPU temperature   : `vcgencmd measure_temp`"
-echo -e "\nFirmware version : `vcgencmd version`"
-echo -e "\nCoedcs status:"
-echo "`vcgencmd codec_enabled H264`"
-echo "`vcgencmd codec_enabled MPG2`"
-echo "`vcgencmd codec_enabled WVC1`"
-echo -e "\n#####################################\n"
+echo "$(tput setaf 2)
+     .~~.   .~~.       $(tput setaf 6)Processors           : `cat /proc/cpuinfo | grep '^processor' | wc -l`$(tput setaf 2)
+    '. \ ' ' / .'      $(tput setaf 6)Serial               : `cat /proc/cpuinfo | grep '^Serial' | awk '{ printf($3) }'`$(tput setaf 1)
+     .~ .~~~..~.       $(tput setaf 6)Hardware             : `cat /proc/cpuinfo | grep '^Hardware' | awk '{ printf($3) }'`$(tput setaf 1)
+    : .~.'~'.~. :      $(tput setaf 6)Revision             : `cat /proc/cpuinfo | grep '^Revision' | awk '{ printf($3) }'`$(tput setaf 1)
+   ~ (   ) (   ) ~     $(tput setaf 6)CORE / ARM frequency : `vcgencmd measure_clock core | awk -F= '/=/{print($1)}'`$(tput setaf 1)
+  ( : '~'.~.'~' : )    $(tput setaf 6)CORE voltage         : `vcgencmd measure_volts core`$(tput setaf 1)
+   ~ .~ (   ) ~. ~     $(tput setaf 6)CPU temperature      : `vcgencmd measure_temp`$(tput setaf 1)
+    (  : '~' :  )      $(tput setaf 6)IP / Hostname        : `/sbin/ifconfig eth0 | /bin/grep "inet addr" | /usr/bin/cut -d ":" -f 2 | /usr/bin/cut -d " " -f 1` / `hostname -f`$(tput setaf 1)
+     '~ .~~~. ~'       $(tput setaf 6)Firmware date        : `vcgencmd version | head -1`$(tput setaf 1)
+         '~'           $(tput setaf 6)Firmware version     : `vcgencmd version | grep ^version | awk '{ print($2) }'`
+                       Codecs enabled   : `for codec in H264 MPG2 WVC1 MPG4 MJPG WMV9; do echo "$(vcgencmd codec_enabled $codec)"; done | grep enabled | awk -F= '/=/{print $1}' | tr '\n' ' '`
+                       Codecs disabled  : `for codec in H264 MPG2 WVC1 MPG4 MJPG WMV9; do echo "$(vcgencmd codec_enabled $codec)"; done | grep disabled | awk -F= '/=/{print $1}' | tr '\n' ' '`
+$(tput sgr0)"
